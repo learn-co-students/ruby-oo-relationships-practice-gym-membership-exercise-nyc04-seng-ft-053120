73a7881a2d433d51c8e1b56014a059979f8cca94
AlveeM <alvee.akand@outlook.com>
2020-06-27 14:49:49 -0400

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,39 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
+  end
+
+  def memberships
+    Membership.all.select { |membership| membership.lifter == self }
+  end
+
+  def sign_up(cost, gym)
+    Membership.new(cost, self, gym)
+  end
+
+  def gyms
+    self.memberships.map { |membership| membership.gym }
+  end
+
+  def total_cost
+    self.memberships.reduce(0) { |sum, membership| sum += membership.cost }
+  end
+
+  def self.lift_sum
+    self.all.reduce(0) { |sum, lifter| sum += lifter.lift_total }
+  end
+
+  def self.avg_lift_total
+    self.lift_sum.fdiv(self.all.length)
+  end
+
+  def self.all 
+    @@all
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,20 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+g1 = Gym.new("Cake")
+g2 = Gym.new("Donut")
+g3 = Gym.new("Tart")
+
+l1 = Lifter.new("Rock", 200)
+l2 = Lifter.new("Stone", 300)
+l3 = Lifter.new("Pebble", 100)
+
+l1.sign_up(50, g1)
+l1.sign_up(30, g2)
+l2.sign_up(40, g3)
+l2.sign_up(50, g1)
+l3.sign_up(20, g2)
+
 binding.pry
 
 puts "Gains!"
